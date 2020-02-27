06b5086858cb2d191736572883670dcbd430b65e
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-27 19:00:12 +0400

    002_c

--- _First_Entity/001_Entity/Program.cs
+++ _First_Entity/001_Entity/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _001_Entity.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,27 @@ namespace _001_Entity
     {
         static void Main(string[] args)
         {
+            using (UserContext db = new UserContext())
+            {
+                // создаем два объекта User
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Sam", Age = 26 };
+
+                // добавляем их в бд
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var users = db.Users;
+                Console.WriteLine("Список объектов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine("{0}.{1} - {2}", u.Id, u.Name, u.Age);
+                }
+            }
+            Console.Read();
         }
     }
 }
